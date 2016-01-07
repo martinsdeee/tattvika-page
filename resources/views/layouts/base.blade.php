91de0dfa3776a783c0b2c1abb934feea378ea0da
Martins Dumbris <martins.dumbris@gmail.com>
2016-01-07 12:53:17 +0200

    Use https instead of http

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,7 +3,7 @@
 <head>
     <title>@yield('title', Config::get('template.product'))</title>
     <!--Import Google Icon Font-->
-    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.4/css/materialize.min.css">
 
@@ -15,7 +15,7 @@
 
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.4/js/materialize.min.js"></script>
-    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/vue/1.0.4/vue.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.4/vue.min.js"></script>
     
     @include('partials.scripts')
 </body>
